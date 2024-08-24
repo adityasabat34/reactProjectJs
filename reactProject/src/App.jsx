f8f1f3ce6a8eea77ab507125ab83bc289b9a4bab
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2024-08-24 07:05:42 +0530

    data fetching of single job done using routerDom in a cleaner way

--- reactProject/src/App.jsx
+++ reactProject/src/App.jsx
@@ -8,7 +8,7 @@ import MainLayouts from './layouts/MainLayouts';
 import Homepages from './screens/Homepages';
 import JobsScreen from './screens/JobsScreen';
 import NotFoundScreen from './screens/NotFoundScreen';
-import JobScreen from './screens/JobScreen';
+import JobScreen, { jobLoader } from './screens/JobScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -16,7 +16,7 @@ const router = createBrowserRouter(
       <Route index element={<Homepages />} />
       <Route path="/jobs" element={<JobsScreen />} />
       <Route path="*" element={<NotFoundScreen />} />
-      <Route path="/jobs/:id" element={<JobScreen />} />
+      <Route path="/jobs/:id" element={<JobScreen />} loader={jobLoader} />
     </Route>
   )
 );
--- reactProject/src/screens/JobScreen.jsx
+++ reactProject/src/screens/JobScreen.jsx
@@ -1,40 +1,18 @@
-import { useEffect, useState } from 'react';
 import Loader from '../components/Loader';
-import { useParams } from 'react-router-dom';
+import { useParams, useLoaderData } from 'react-router-dom';
 
 const JobScreen = () => {
   const { id } = useParams();
+  const job = useLoaderData();
+  console.log(job);
 
-  const [job, setJob] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchSingleJobDetails = async () => {
-      try {
-        const res = await fetch(`/api/jobs/${id}`);
-        console.log('code reaches here');
-        if (!res.ok) {
-          throw new Error(`HTTP error! status: ${res.status}`);
-        }
-        const data = await res.json();
-        console.log(data);
-        setJob(data);
-      } catch (error) {
-        console.log('HTTP req error', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchSingleJobDetails();
-  }, []);
-
-  return (
-    <div>
-      <>
-        {loading ? <Loader /> : <h2 className="text-center"> {job.title}</h2>}
-      </>
-    </div>
-  );
+  return <></>;
 };
 
-export default JobScreen;
+const jobLoader = async ({ params }) => {
+  const res = await fetch(`/api/jobs/${params.id}`);
+  const data = res.json();
+  return data;
+};
+
+export { JobScreen as default, jobLoader };
